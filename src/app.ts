1edc25c089823bdfc0c3927a8a47d0f546f79744
miavai649 <mahmudulhaquenoor@gmail.com>
2023-05-18 17:32:03 +0600

    https://brokescholar.com

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,46 @@ app.get('/wativ', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello World!')
 })
 
+// https://brokescholar
+app.get('/brokescholar', (req: Request, res: Response, next: NextFunction) => {
+  puppeteer.use(StealthPlugin())
+  const coupons : object[] = [];
+  // puppeteer usage as normal
+  puppeteer.launch({
+    headless: false,
+    executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+    userDataDir: 'C:/Users/mahmu/AppData/Local/Google/Chrome/User Data/Default'
+  }).then(async browser => {
+    console.log('Running tests..')
+    const page = await browser.newPage()
+    page.setDefaultNavigationTimeout(20 * 60 * 1000)
+    const targetUrl = 'https://brokescholar.com/coupon-codes/doe-lashes' 
+    await page.goto(targetUrl)
+    const data_href = await page.$$eval('li .card', el => el.map(href => href.getAttribute("data-href")))
+   const filterHref = data_href.filter(href => href !== null)
+    const finalHref = filterHref.map(href => href?.split('=')[1]).filter(code => code !== undefined)
+    for (const href of finalHref) {
+      await page.goto(`${targetUrl}?deal=${href}`, {
+        waitUntil: 'networkidle0'
+      })
+      // console.log(page.url());
+      const elementHandle = await page.waitForSelector(`.modal`)
+      const coupon = elementHandle && await elementHandle.evaluate(() => {
+        const couponCode = document.querySelector('.copy-code .code button')?.getAttribute('data-clipboard-text');
+        return {
+          couponCode
+        }
+      })
+      if (coupon?.couponCode) coupons.push(coupon);
+    }
+    console.log(coupons);
+    await browser.close()
+    console.log(`All done, check the result. ✨`)
+  })
+
+  res.send('Hello World!')
+})
+
 
 
 
